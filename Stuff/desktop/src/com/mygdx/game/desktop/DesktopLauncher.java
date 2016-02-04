91aaefa07d8a965cebedde80e495a74d526a1b68
Dario Rahimic <dario.rahimic@hotmail.com>
2016-02-04 10:54:55 +0100

    Stuff from Dario

--- Stuff/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ Stuff/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -1,12 +1,12 @@
-package com.vr.cube.desktop;
+package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.vr.cube.Cube;
+import com.mygdx.game.MyGdxGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Cube(), config);
+		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
