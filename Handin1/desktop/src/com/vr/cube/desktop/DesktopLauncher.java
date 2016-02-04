bca34e7d266750c230452ce9f48b25d28368fe53
sletgaard <sunesletgaard@gmail.com>
2016-02-04 10:45:41 +0100

    Initial commit

---
+++ Handin1/desktop/src/com/vr/cube/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.vr.cube.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.vr.cube.Cube;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Cube(), config);
+	}
+}
